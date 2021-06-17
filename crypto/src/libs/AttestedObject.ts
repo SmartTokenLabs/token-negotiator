6ffb47b5d2bac009d2bf52b410756c4a3f6dea3a
Nick Taras <nicktaras@hotmail.co.uk>
2021-06-17 11:09:01 +1000

    updated crypto to the dir located in crypto-javascript (latest)

--- crypto/src/libs/AttestedObject.ts
+++ crypto/src/libs/AttestedObject.ts
@@ -1,98 +1,217 @@
 import {AttestationCrypto} from "./AttestationCrypto";
-import {SignedAttestation} from "./SignedAttestation";
-import {uint8tohex} from "./utils";
+import {SignedIdentityAttestation} from "./SignedIdentityAttestation";
+import {hexStringToArray, uint8toBuffer, uint8tohex} from "./utils";
 import {Asn1Der} from "./DerUtility";
-import {AttestableObject} from "./AttestableObject";
 import {ProofOfExponentInterface} from "./ProofOfExponentInterface";
+import {KeyPair} from "./KeyPair";
+import {AsnParser} from "@peculiar/asn1-schema";
+import {UseToken} from "../asn1/shemas/UseToken";
+import {UsageProofOfExponent} from "./UsageProofOfExponent";
+import {IdentifierAttestation} from "./IdentifierAttestation";
+import {Attestable} from "./Attestable";
+import {SignatureUtility} from "./SignatureUtility";
+import {Verifiable} from "./Verifiable";
+import {ASNEncodable} from "./ASNEncodable";
+import {AttestableObject} from "./AttestableObject";
 
 declare global {
-    interface Window { ethereum: any; }
+    interface Window {
+        ethereum: any;
+        web3: any;
+    }
 }
 
-// TODO public AttestedObject(T object, SignedAttestation att, ProofOfExponent pok, byte[] signature,
-//       AsymmetricKeyParameter userPublicKey) {
-export class AttestedObject {
+export class AttestedObject implements ASNEncodable, Verifiable {
     private crypto: AttestationCrypto;
     private pok: ProofOfExponentInterface;
     private unsignedEncoding: string;
     private derEncodedProof: string;
     private signature: string;
     private encoding: string;
-    constructor(
-        private attestableObject: AttestableObject,
-        private att: SignedAttestation,
-        private attestationSecret: bigint ,
-        private objectSecret: bigint
-    ) {
+    private attestableObject: any;
+    private att: SignedIdentityAttestation;
+    private attestationSecret: bigint ;
+    private objectSecret: bigint;
+    private userPublicKey: Uint8Array;
+    private userKeyPair: KeyPair;
+
+    private preSignEncoded: string;
+
+    private webDomain: string;
+
+    static Eip712UserData: {[index: string]:string|number}  = {
+        payload: '',
+        description: '',
+        timestamp: 0
+    }
+    // static Eip712UserDataTypes: {[index: string]:string}[]  = [
+    static Eip712UserDataTypes: {name: string, type: string}[]  = [
+        {name: 'payload', type: 'string'},
+        {name: 'description', type: 'string'},
+        {name: 'timestamp', type: 'uint256'},
+    ]
+    static Eip712UserDataPrimaryName: string = "Authentication";
+    static Eip712UserDataDescription: string = "Single-use authentication";
+
+    constructor() {}
+
+    create<T extends Attestable>(
+        attestableObject: T ,
+        att: SignedIdentityAttestation,
+        attestationSecret: bigint ,
+        objectSecret: bigint
+    ){
+        this.attestableObject = attestableObject;
+        this.att = att;
+        this.attestationSecret = attestationSecret;
+        this.objectSecret = objectSecret;
         this.crypto = new AttestationCrypto();
         this.pok = this.makeProof(attestationSecret, objectSecret, this.crypto);
         this.derEncodedProof = this.pok.getDerEncoding();
 
-        let vec =
-            this.attestableObject.getDerEncoding() +
+        this.fillPresignData();
+
+    }
+
+    setWebDomain(domain: string){
+        this.webDomain = domain;
+    }
+
+    fillPresignData(){
+
+        this.preSignEncoded = this.attestableObject.getDerEncoding() +
             this.att.getDerEncoding() +
             this.pok.getDerEncoding();
-        this.unsignedEncoding = Asn1Der.encode('SEQUENCE_30', vec);
+        this.unsignedEncoding = Asn1Der.encode('SEQUENCE_30', this.preSignEncoded);
     }
-/*
-    public async signFinalObject(){
-        let vec =
-            uint8tohex(this.attestableObject.getDerEncoding()) +
-            uint8tohex(this.att.getDerEncoding())+
-            this.pok.getDerEncoding();
-        this.unsignedEncoding = Asn1Der.encode('SEQUENCE_30', vec);
-        const hash = await ethers.utils.keccak256(hexStringToArray(this.unsignedEncoding));
 
-        console.log('hash');
-        console.log(hash);
+    fromDecodedData<T extends Attestable>(
+        attestableObject: T ,
+        att: SignedIdentityAttestation,
+        pok: ProofOfExponentInterface ,
+        signature: string
+    ){
+        this.attestableObject = attestableObject;
+        this.att = att;
+        this.pok = pok;
+        this.signature = signature;
 
-        // TODO sign by user wallet
-        // this.signature = SignatureUtility.sign(this.unsignedEncoding, userKeys.getPrivate());
-        if (!window.ethereum){
-            throw new Error('Please install metamask before.');
+        this.fillPresignData();
+
+        let vec = this.preSignEncoded + Asn1Der.encode('BIT_STRING', this.signature)
+
+        this.encoding = Asn1Der.encode('SEQUENCE_30', vec);
+        this.userKeyPair = this.att.getUnsignedAttestation().getSubjectPublicKeyInfo();
+
+        if (!this.verify()) {
+            throw new Error("The redeem request is not valid");
         }
-
-        const provider = new ethers.providers.Web3Provider(window.ethereum);
-        const signer = provider.getSigner();
-        if (!signer) throw new Error("Active Wallet required");
-
-        const userAddress = await signer.getAddress();
-
-        console.log('lets sign message');
-        const metamaskEnabled = await window.ethereum.enable();
-
-        if (!metamaskEnabled){
-            throw new Error("Active Wallet required");
-        }
-
-        // console.log(this.unsignedEncoding);
-
-        // let signature = await signer.signMessage(hexStringToArray(this.unsignedEncoding));
-        let signature = await signer.signMessage(ethers.utils.arrayify(hash));
-        console.log('signature');
-        console.log(signature);
-
-        if (!signature){
-            throw new Error("Cant sign data");
-        }
-
-        const ethereumHash = await ethers.utils.keccak256("\x19Ethereum Signed Message:\n" + hash.length + hash);
-        const pk = ethers.utils.recoverPublicKey(ethereumHash, signature);
-        const recoveredAddress = ethers.utils.computeAddress(ethers.utils.arrayify(pk));
-
-        console.log('recoveredAddress');
-        console.log(recoveredAddress);
-
-        //     vec.add(new DERBitString(this.signature));
-        //     this.encoding = new DERSequence(vec).getEncoded();
-        // } catch (IOException e) {
-        //     throw new RuntimeException(e);
-        // }
-        // if (!verify()) {
-        //     throw new IllegalArgumentException("The redeem request is not valid");
-        // }
     }
-*/
+
+
+    async sign(){
+        let userData = {
+            payload: this.unsignedEncoding,
+            description: AttestedObject.Eip712UserDataDescription,
+            timestamp: new Date().getTime()
+        };
+
+        return await SignatureUtility.signEIP712WithBrowserWallet(this.webDomain, userData, AttestedObject.Eip712UserDataTypes, AttestedObject.Eip712UserDataPrimaryName );
+    }
+
+    public checkValidity(): boolean {
+        // CHECK: that it is an identity attestation otherwise not all the checks of validity needed gets carried out
+        try {
+            let attEncoded = this.att.getUnsignedAttestation().getDerEncoding();
+            let std: IdentifierAttestation = IdentifierAttestation.fromBytes(new Uint8Array(hexStringToArray(attEncoded))) as IdentifierAttestation;
+
+            // CHECK: perform the needed checks of an identity attestation
+            if (!std.checkValidity()) {
+                console.error("The attestation is not a valid standard attestation");
+                return false;
+            }
+        } catch (e) {
+            console.error("The attestation is invalid");
+            return false;
+        }
+
+        // CHECK: that the cheque is still valid
+        if (!this.getAttestableObject().checkValidity()) {
+            console.error("Cheque is not valid");
+            return false;
+        }
+
+        // CHECK: the Ethereum address on the attestation matches receivers signing key
+        let attestationEthereumAddress: string = this.getAtt().getUnsignedAttestation().getSubject().substring(3);
+
+        if (attestationEthereumAddress.toLowerCase() !== KeyPair.publicFromUint(this.getUserPublicKey()).getAddress().toLowerCase()) {
+            console.error("The attestation is not to the same Ethereum user who is sending this request");
+            return false;
+        }
+
+        // CHECK: verify signature on RedeemCheque is from the same party that holds the attestation
+        if (this.signature != null) {
+            let spki = this.getAtt().getUnsignedAttestation().getSubjectPublicKeyInfo();
+            try {
+                if (!spki.verifyHexStringWithEthereum(this.unsignedEncoding, this.signature)) {
+                    console.error("The signature on RedeemCheque is not valid");
+                    return false;
+                }
+            } catch (e) {
+                console.error("The attestation SubjectPublicKey cannot be parsed");
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    verify(): boolean{
+        let result: boolean =
+            this.attestableObject.verify()
+            && this.att.verify()
+            && this.crypto.verifyEqualityProof(
+                this.att.getUnsignedAttestation().getCommitment(),
+                this.attestableObject.getCommitment(),
+                this.pok
+            );
+        if (this.signature != null) {
+            let spki = this.getAtt().getUnsignedAttestation().getSubjectPublicKeyInfo();
+            return result && spki.verifyHexStringWithEthereum(this.unsignedEncoding, this.signature);
+        } else {
+            return result;
+        }
+    }
+
+    static fromBytes<D extends UseToken, T extends AttestableObject>(asn1: Uint8Array, decoder: new () => D, attestorKey: KeyPair, attestable: new () => T, issuerKey: KeyPair): AttestedObject{
+        let attested: D = AsnParser.parse( uint8toBuffer(asn1), decoder);
+
+        // console.log('attested');
+        // console.log(attested);
+
+        let me = new this();
+
+        // let attestableObj: T
+        me.attestableObject = new attestable();
+        me.attestableObject.fromBytes(attested.signedToken, issuerKey);
+
+        me.att = SignedIdentityAttestation.fromBytes(new Uint8Array(attested.attestation), attestorKey);
+
+        let pok = new UsageProofOfExponent();
+        pok.fromBytes( new Uint8Array(attested.proof) ) ;
+        me.pok = pok;
+
+        let attCom: Uint8Array = me.att.getUnsignedAttestation().getCommitment();
+        let objCom: Uint8Array = me.attestableObject.getCommitment();
+        let crypto = new AttestationCrypto();
+
+        if (!crypto.verifyEqualityProof(attCom, objCom, pok)) {
+            throw new Error("The redeem proof did not verify");
+        }
+
+        return me;
+    }
+
     private makeProof(attestationSecret: bigint, objectSecret: bigint, crypto: AttestationCrypto): ProofOfExponentInterface {
         // TODO Bob should actually verify the attestable object is valid before trying to cash it to avoid wasting gas
         // Need to decode twice since the standard ASN1 encodes the octet string in an octet string
@@ -103,11 +222,11 @@ export class AttestedObject {
 
         // TODO we dont parse that value, because its already parsed to this.riddle
         // let attCom: Uint8Array = new Uint8Array(extensions.extension.extnValue);
-        let attCom: Uint8Array = this.att.getUnsignedAttestation().getRiddle();
+        let attCom: Uint8Array = this.att.getUnsignedAttestation().getCommitment();
         let objCom: Uint8Array = this.attestableObject.getCommitment();
         let pok: ProofOfExponentInterface = crypto.computeEqualityProof(uint8tohex(attCom), uint8tohex(objCom), attestationSecret, objectSecret);
 
-        if (!crypto.verifyEqualityProof(uint8tohex(attCom), uint8tohex(objCom), pok)) {
+        if (!crypto.verifyEqualityProof(attCom, objCom, pok)) {
             throw new Error("The redeem proof did not verify");
         }
         return pok;
@@ -127,8 +246,11 @@ export class AttestedObject {
 
     public getDerEncodingWithSignature() { return this.encoding; }
 
-    // TODO type it
-    public getDerEncoding() {
+    public getDerEncoding():string {
         return this.unsignedEncoding;
+    }
+
+    public getUserPublicKey() {
+        return this.userPublicKey;
     }
 }
--- crypto/src/libs/IdentifierAttestation.ts
+++ crypto/src/libs/IdentifierAttestation.ts
@@ -1,61 +1,44 @@
 import {KeyPair} from "./KeyPair";
 import {AttestationCrypto} from "./AttestationCrypto";
-import {Asn1Der} from "./DerUtility";
-import {uint8tohex} from "./utils";
 import {Attestation} from "./Attestation";
+import {Validateable} from "./Validateable";
 
-export class IdentifierAttestation extends Attestation {
+export class IdentifierAttestation extends Attestation implements Validateable{
     private crypto: AttestationCrypto;
     static OID_OCTETSTRING = "1.3.6.1.4.1.1466.115.121.1.40";
-    constructor(riddle: Uint8Array, private keys: KeyPair) {
+    private DEFAULT_SIGNING_ALGORITHM = "1.2.840.10045.4.2";
+
+    constructor() {
         super();
+    }
+
+    fromCommitment(commitment: Uint8Array, keys: KeyPair){
+
+        this.subjectKey = keys;
         this.setVersion(18); // Our initial version
-        this.setSubject("CN=" + this.keys.getAddress());
-        this.setSignature(AttestationCrypto.OID_SIGNATURE_ALG);
+        this.setSubject("CN=" + this.subjectKey.getAddress());
+        this.setSigningAlgorithm(this.DEFAULT_SIGNING_ALGORITHM);
 
         this.setSubjectPublicKeyInfo(keys);
-        this.setRiddle(riddle);
+        this.setCommitment(commitment);
     }
 
     static fromData(identity: string, type: number, keys: KeyPair, secret: bigint){
         let crypto = new AttestationCrypto();
-        let riddle = crypto.makeCommitment(identity, type, secret);
-        return new this(riddle, keys);
+        let commitment = crypto.makeCommitment(identity, type, secret);
+        return (new this()).fromCommitment(commitment, keys);
     }
 
-    setSignature(signature: string) {
-        // TODO create algorithm parser and change variable type
-        // let alg = new AlgorithmIdentifierASN();
-        // alg.algorithm = signature;
-        // hadrcoded ASN1 sequence
-        this.signature = "300906072A8648CE3D0201";
+    static fromBytes(bytes: Uint8Array){
+        return super.fromBytes(bytes);
     }
+
     setSubjectPublicKeyInfo(keys: KeyPair){
-        // TODO generate algorithm object
-        // hardcoded algorithm
-        // let hardcodedAlg = "3081EC06072A8648CE3D02013081E0020101302C06072A8648CE3D0101022100FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F3044042000000000000000000000000000000000000000000000000000000000000000000420000000000000000000000000000000000000000000000000000000000000000704410479BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8022100FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141020101";
-
-        // let res = hardcodedAlg + Asn1Der.encode('BIT_STRING', keys.getPublicKeyAsHexStr());
-        // this.subjectPublicKeyInfo = Asn1Der.encode('SEQUENCE_30', res);
-        this.subjectPublicKey = keys;
+        this.subjectKey = keys;
     }
 
-
-    setRiddle(riddle: Uint8Array) {
-        // TODO hardcoded OID
-        // extensions.add(new ASN1ObjectIdentifier(Attestation.OID_OCTETSTRING));
-        let attestOIDencoded = "060B2B060104018B3A73790128";
-        let extensions: string = attestOIDencoded
-            + Asn1Der.encode('BOOLEAN', 1)
-        + Asn1Der.encode('OCTET_STRING', uint8tohex(riddle));
-
-        let extensionsEncoded = Asn1Der.encode('SEQUENCE', extensions);
-        extensionsEncoded = Asn1Der.encode('SEQUENCE', extensionsEncoded);
-
-        this.riddle = riddle;
-        // Double Sequence is needed to be compatible with X509V3
-        // TODO create extensions as correct type "Extensions"
-        // this.extensions = extensionsEncoded;
+    setCommitment(encodedRiddle: Uint8Array) {
+        this.commitment = encodedRiddle;
     }
 
     checkValidity(): boolean {
@@ -70,18 +53,36 @@ export class IdentifierAttestation extends Attestation {
             console.error("The subject is supposed to only be an Ethereum address as the Common Name");
             return false;
         }
-        if (this.getSignature() != AttestationCrypto.OID_SIGNATURE_ALG) {
-            console.error("The signature algorithm is supposed to be " + AttestationCrypto.OID_SIGNATURE_ALG);
-            return false;
-        }
+        // TODO check if we really need to skip that check
+        // if (this.getSigningAlgorithm() != AttestationCrypto.OID_SIGNATURE_ALG) {
+        //     console.error("The signature algorithm is supposed to be " + AttestationCrypto.OID_SIGNATURE_ALG);
+        //     return false;
+        // }
         // Verify that the subject public key matches the subject common name
 
-        let parsedSubject: string = "CN=" + this.subjectPublicKey.getAddress();
-        if (parsedSubject != this.getSubject()) {
+        let parsedSubject: string = "CN=" + this.subjectKey.getAddress();
+        if (parsedSubject.toLowerCase() != this.getSubject().toLowerCase()) {
             console.error("The subject public key does not match the Ethereum address attested to");
             return false;
         }
 
         return true;
+    }
+
+    setIssuer(issuer: string){
+        this.issuer = issuer;
+    }
+
+    public getSerialNumber(): number {
+        return this.serialNumber;
+    }
+
+    public setSerialNumber(serialNumber: number) {
+        this.serialNumber = serialNumber;
+    }
+
+    public getAddress(): string {
+        // Remove the "CN=" prefix
+        return this.subjectKey.getAddress();
     }
 }
