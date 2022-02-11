de49dc7feb2c773dbfd10de49493583428ffff3e
Nick Taras <nicktaras@Nicks-MacBook-Pro-2.local>
2022-02-11 15:00:28 -0700

    added unit tests

--- src/utils/__tests__/util.spec.ts
+++ src/utils/__tests__/util.spec.ts
@@ -1,4 +1,4 @@
-import { compareObjects, base64ToUint8array } from './../index';
+import { logger, requiredParams, compareObjects, base64ToUint8array } from './../index';
 
 // TODO: add unit tests for the following functions:
 // logger, requiredParams, asyncHandle, attachPostMessageListener.
@@ -33,5 +33,26 @@ describe('util Spec object comparison', () => {
 describe('util Spec base64ToUint8array', () => {
   test('expect base64 to be Uint8array', () => {
     expect(base64ToUint8array('RVJDNzIxLU5vbi1GdW5naWJsZS1Ub2tlbg==').toString()).toEqual([69, 82, 67, 55, 50, 49, 45, 78, 111, 110, 45, 70, 117, 110, 103, 105, 98, 108, 101, 45, 84, 111, 107, 101, 110].toString());
+  });
+});
+describe('util logging and errors', () => {
+  test('expect error to be thrown', () => {
+    const msg = 'In order to have a decentralised database, you need to have security. In order to have security, you need to - you need to have incentives.';
+    const item = null;
+    try {
+      requiredParams(item, msg);
+      // Fail test if above expression doesn't throw anything.
+      expect(true).toBe(false);
+    } catch (e) {
+      // Error Caught
+      expect(true).toBe(true);
+    }
+  });
+  test('expect logger', () => {
+    console.log = jest.fn();
+    const msg = "It's the possibility of having a dream come true that makes life interesting.";
+    logger(msg);
+    // The first argument of the first call to the function was 'hello'
+    expect(console.log).toHaveBeenCalledWith(msg);
   });
 });
