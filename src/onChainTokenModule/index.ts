c1f0b38eb3d972c353b5cb13516de62b822437ce
micwallace <micwallace@gmx.com>
2022-04-04 13:14:36 +1000

    Fix wrong variable name.

--- src/onChainTokenModule/index.ts
+++ src/onChainTokenModule/index.ts
@@ -71,7 +71,7 @@ export class OnChainTokenModule {
         if (contract.toLowerCase() == "0x22c1f6050e56d2876009903609a2cc3fef83b415"){
             return {
                 chain,
-                address,
+                contract,
                 emblem: "https://storage.googleapis.com/subgraph-images/1647414847706poap.jpeg",
                 title: "POAP Proof of attendance protocol"
             }
