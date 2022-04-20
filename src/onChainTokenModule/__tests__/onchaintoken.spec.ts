988478b6328d300a58df99a9fd087af1e980a770
Sam Warwick <sam@samwarwick.com>
2022-04-21 09:27:27 +1000

    Add initial onChainTokenModule tests. Includes fix for bugs in getOnChainAPISupportBool

---
+++ src/onChainTokenModule/__tests__/onchaintoken.spec.ts
@@ -0,0 +1,17 @@
+import { OnChainTokenModule } from "../index";
+
+describe('On-chain token module', () => {
+
+  test('getOnChainAPISupportBool should be true for supported APIs and chains', () => {
+    const token = new OnChainTokenModule();
+    expect(token.getOnChainAPISupportBool('opensea', 'eth')).toBe(true);
+    expect(token.getOnChainAPISupportBool('alchemy', 'polygon')).toBe(true);
+  });
+
+  test('getOnChainAPISupportBool should be false for unsupported APIs and chains', () => {
+    const token = new OnChainTokenModule();
+    expect(token.getOnChainAPISupportBool('nosuchapi', 'eth')).toBe(false); // throws exception
+    expect(token.getOnChainAPISupportBool('opensea', 'nosuchchain')).toBe(false); // returns true
+  });
+
+});
