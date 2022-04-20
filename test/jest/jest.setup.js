34f931b64def8f160324841ca4a51370181f4ef3
Sam Warwick <sam@samwarwick.com>
2022-04-21 09:04:37 +1000

    Fix eslint errors in jest configs

--- test/jest/jest.setup.js
+++ test/jest/jest.setup.js
@@ -1,5 +1,6 @@
+// eslint-disable-next-line no-undef
 jest.spyOn(global.console, 'error').mockImplementation((message) => {
   if (!message.toString().startsWith('ReferenceError: fetch is not defined')) {
     throw new Error(`Unexpected console.error: ${message}`);
-  }  
+  }
 });
--- test/jest/jest.transform.js
+++ test/jest/jest.transform.js
@@ -3,4 +3,6 @@ const babelOptions = {
         'allowReturnOutsideFunction': true
     },
 };
+
+// eslint-disable-next-line no-undef, @typescript-eslint/no-var-requires
 module.exports = require('babel-jest').default.createTransformer(babelOptions);
