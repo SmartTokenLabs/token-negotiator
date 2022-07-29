66fe63c619ba6a0e78f70fcd809185cfec981ddd
Nick Taras <nicktaras@Nicks-MacBook-Pro-3.local>
2022-07-30 01:07:42 +1000

    initial integration complete for solana.

--- src/utils/token/nftProvider.ts
+++ src/utils/token/nftProvider.ts
@@ -1,40 +1,85 @@
 // @ts-nocheck
 import { OnChainTokenConfig } from "../../client/interface";
 
-const baseURL = "https://api.token-discovery.tokenscript.org"
+const baseURL = "http://localhost:3000";
+// const baseURL = "https://api.token-discovery.tokenscript.org";
 
 export const getNftCollection = async (
   issuer: OnChainTokenConfig,
-	ipfsBaseUrl?: string
+  ipfsBaseUrl?: string
 ) => {
-	const { contract, network, openSeaSlug } = issuer;
-	const blockchain = issuer?.blockchain ?? "ethereum";
-	let query = `${baseURL}/get-token-collection?smartContract=${contract}&network=${network}blockchain=${blockchain}&ipfsBaseUrl=${ipfsBaseUrl}`;
-	if(openSeaSlug) query += `&openSeaSlug=${openSeaSlug}`;
-	return tokenRequest(query, true);
+  const blockchain = issuer?.blockchain ?? "ethereum";
+  let query: string;
+  if (blockchain === "solana") {
+    query = getSolanaNftCollectionUrl(issuer, ipfsBaseUrl);
+	} else {
+		query = getEvmNftCollectionUrl(issuer, ipfsBaseUrl);
+	}
+  return tokenRequest(query, true);
 };
 
-export const getNftTokens = async (
+const getEvmNftCollectionUrl = (issuer: any, ipfsBaseUrl: string) => {
+  const { contract, chain, openSeaSlug } = issuer;
+  let query = `${baseURL}/get-token-collection?smartContract=${contract}&chain=${chain}&blockchain=ethereum`;
+  if (openSeaSlug) query += `&openSeaSlug=${openSeaSlug}`;
+  if (ipfsBaseUrl) query += `&ipfsBaseUrl=${ipfsBaseUrl}`;
+  return query;
+};
+
+const getSolanaNftCollectionUrl = (issuer: any, ipfsBaseUrl: string) => {
+  const { collectionId, chain } = issuer;
+  let query = `${baseURL}/get-token-collection?collectionId=${collectionId}&chain=${chain}&blockchain=solana`;
+  if (ipfsBaseUrl) query += `&ipfsBaseUrl=${ipfsBaseUrl}`;
+  return query;
+};
+
+export const getNftTokens = (
   issuer: OnChainTokenConfig,
   owner: string,
-	ipfsBaseUrl?: string
+  ipfsBaseUrl?: string
 ) => {
-	const { contract, network, openSeaSlug } = issuer;
 	const blockchain = issuer?.blockchain ?? "ethereum";
-	let query = `${baseURL}/get-owner-tokens?smartContract=${contract}&network=${network}&owner=${owner}&blockchain=${blockchain}&ipfsBaseUrl=${ipfsBaseUrl}`;
-	if(openSeaSlug) query += `&openSeaSlug=${openSeaSlug}`;
-	return tokenRequest(query, false);
+  let query: string;
+  if (blockchain === "solana") {
+    query = getSolanaNftTokensUrl(issuer, owner, ipfsBaseUrl);
+	} else {
+		query = getEvmNftTokensUrl(issuer, owner, ipfsBaseUrl);
+	}
+  return tokenRequest(query, true);
 };
 
-export const tokenRequest = async (query:string, silenceRequestError:boolean) => {
-	try {
-		const response = await fetch(query);
-		const ok = (response.status >= 200 && response.status <= 299) ? true : false;
-		if (!ok && silenceRequestError === true) { console.warn('token api request failed: ', query); return; }
-		if (ok) return response.json();
-		else throw new Error(`HTTP error! status: ${response.status}`);		
-	} catch (msg:any) {
-		throw new Error(`HTTP error.`);
-	}
+const getEvmNftTokensUrl = (issuer:any, owner:string, ipfsBaseUrl:string) => {
+	const { contract, chain, openSeaSlug } = issuer;
+  const blockchain = issuer?.blockchain ?? "ethereum";
+  let query = `${baseURL}/get-owner-tokens?smartContract=${contract}&chain=${chain}&owner=${owner}&blockchain=${blockchain}`;
+  if (openSeaSlug) query += `&openSeaSlug=${openSeaSlug}`;
+	if (ipfsBaseUrl) query += `&ipfsBaseUrl=${ipfsBaseUrl}`;
+	return query;
 }
 
+const getSolanaNftTokensUrl = (issuer:any, owner:string, ipfsBaseUrl:string) => {
+	const { chain, collectionSymbol } = issuer;
+  const blockchain = issuer?.blockchain ?? "ethereum";
+  let query = `${baseURL}/get-owner-tokens?chain=${chain}&owner=${owner}&blockchain=${blockchain}`;
+  if (collectionSymbol) query += `&collectionSymbol=${collectionSymbol}`;
+	if (ipfsBaseUrl) query += `&ipfsBaseUrl=${ipfsBaseUrl}`;
+	return query;
+}
+
+export const tokenRequest = async (
+  query: string,
+  silenceRequestError: boolean
+) => {
+  try {
+    const response = await fetch(query);
+    const ok = response.status >= 200 && response.status <= 299 ? true : false;
+    if (!ok && silenceRequestError === true) {
+      console.warn("token api request failed: ", query);
+      return;
+    }
+    if (ok) return response.json();
+    else throw new Error(`HTTP error! status: ${response.status}`);
+  } catch (msg: any) {
+    throw new Error(`HTTP error.`);
+  }
+};
