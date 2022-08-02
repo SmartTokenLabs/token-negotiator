a8d9a1df2488b8d78b6c3ab54482e33d5a405de1
IntelMin0915 <intelmin0915@gmail.com>
2022-08-02 16:33:52 -0400

    remove auth.spec.ts

--- src/client/__tests__/auth.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AuthenticationMethod, AuthenticationResult, AbstractAuthentication } from "../auth/abstractAuthentication";
-
-describe('auth spec', () => {
-	test('client/auth test', () => {
-		console.log("started..");
-	});
-});
-
