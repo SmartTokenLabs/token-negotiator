ac79c9987271b9bb8937b14e66fc4a6b014b2f3f
Michael Wallace <micwallace@gmx.com>
2022-08-02 14:49:19 +1000

    chore: revert test build

--- dist/utils/support/isSupported.js
+++ dist/utils/support/isSupported.js
@@ -1,6 +1,7 @@
 import { getBrowserData } from './getBrowserData';
 export var isUserAgentSupported = function (unSupportedUserAgents) {
-    if (unSupportedUserAgents === void 0) { unSupportedUserAgents = {}; }
+    if (!unSupportedUserAgents)
+        return true;
     var browserData = getBrowserData();
     var unsupported = Object.keys(unSupportedUserAgents);
     return !(unsupported.some(function (browser) { return browserData[browser]; }));
