3a956092d07728b752433b344492e406cc017c38
Nick Taras <nicktaras@Nicks-MacBook-Pro-3.local>
2022-09-27 00:05:05 +1000

    added referrer to localstorage

--- src/outlet/auth-handler.ts
+++ src/outlet/auth-handler.ts
@@ -265,7 +265,7 @@ export class AuthHandler {
 				document.location.href = this.attestationOrigin + "#" + params.toString();
 
 				if(!document.location.href.includes(this.attestationOrigin)) { 
-					localStorage.setItem('attesation-referrer', document.referrer);
+					localStorage.setItem('attestation-referrer', document.referrer);
 				}
 
 				return;
