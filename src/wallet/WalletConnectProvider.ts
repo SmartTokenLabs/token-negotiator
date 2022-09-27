80890d22bf1c5461b2c375f47041ce887300a1fc
snowwhitedev <snowwhite91924@gmail.com>
2022-09-27 14:56:27 +0700

    fix: add wc mumbai rpc url

--- src/wallet/WalletConnectProvider.ts
+++ src/wallet/WalletConnectProvider.ts
@@ -1,5 +1,11 @@
 import WalletConnectProvider from "@walletconnect/web3-provider";
 
 export const getWalletConnectProviderInstance = async (checkConnectionOnly?: boolean) => {
-  return new WalletConnectProvider({ infuraId: "7753fa7b79d2469f97c156780fce37ac", qrcode: !checkConnectionOnly });
+	return new WalletConnectProvider({
+		infuraId: "7753fa7b79d2469f97c156780fce37ac",
+		qrcode: !checkConnectionOnly,
+		rpc: {
+			80001: 'https://polygon-mumbai.g.alchemy.com/v2/rVI6pOV4irVsrw20cJxc1fxK_1cSeiY0'
+		}
+	});
 }
