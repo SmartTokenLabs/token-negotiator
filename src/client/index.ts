3cf096352138a15e42ec719e82dc8ad295da57ec
Michael Wallace <micwallace@gmx.com>
2022-09-29 23:43:30 +1000

    Update version & build

--- src/client/index.ts
+++ src/client/index.ts
@@ -17,7 +17,7 @@ import Web3WalletProvider from '../wallet/Web3WalletProvider';
 import {LocalOutlet} from "../outlet/localOutlet";
 import {OutletInterface} from "../outlet";
 
-if(typeof window !== "undefined") window.tn = { version: "2.2.0-dc.4" };
+if(typeof window !== "undefined") window.tn = { version: "2.2.0-dc.6" };
 
 declare global {
 	interface Window {
