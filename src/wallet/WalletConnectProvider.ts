1a39cac8c991450f472e1b078fdcdd1024917df2
snowwhitedev <snowwhite91924@gmail.com>
2022-11-01 01:00:46 +0700

    feat: rpc config for wallet connect provider set up

--- src/wallet/WalletConnectProvider.ts
+++ src/wallet/WalletConnectProvider.ts
@@ -5,7 +5,13 @@ export const getWalletConnectProviderInstance = async (checkConnectionOnly?: boo
 		infuraId: "7753fa7b79d2469f97c156780fce37ac",
 		qrcode: !checkConnectionOnly,
 		rpc: {
-			80001: 'https://polygon-mumbai.g.alchemy.com/v2/rVI6pOV4irVsrw20cJxc1fxK_1cSeiY0'
+			137: 'https://polygon-rpc.com/', // Polygon
+			56: 'https://bsc-dataseed.binance.org/', // BSC,
+			43114: 'https://api.avax.network/ext/bc/C/rpc', // Avalanche
+			250: 'https://rpc.fantom.network/', // Fantom
+			5: 'https://eth-goerli.g.alchemy.com/v2/yVhq9zPJorAWsw-F87fEabSUl7cCU6z4', // Goerli
+			42161: 'https://arb1.arbitrum.io/rpc', // Arbitrum
+			80001: 'https://polygon-mumbai.g.alchemy.com/v2/rVI6pOV4irVsrw20cJxc1fxK_1cSeiY0' // mumbai
 		}
 	});
 }
