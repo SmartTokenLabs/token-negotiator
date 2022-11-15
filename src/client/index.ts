c69c87c74b870f43b9ddf3e1b88500a3253fda9d
banki louis <bankilouis@gmail.com>
2022-11-15 08:58:05 +0100

    removed comments

--- src/client/index.ts
+++ src/client/index.ts
@@ -410,9 +410,6 @@ export class Client {
 					tokens = await this.loadRemoteOutletTokens(issuerConfig);
 				}
 			} catch (err) {
-				// logger(2,err);
-				// console.log("popup error");
-				// this.eventSender.emitErrorToClient(err, issuer);
 				errorHandler('popup error', 'error', this.eventSender.emitErrorToClient(err, issuer), null, true, false);
 				continue;
 			}
