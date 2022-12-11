d553046505b0e21d77576f805a824905a42d7286
Nick Taras <nicktaras@Nicks-MacBook-Pro-3.local>
2022-12-11 21:59:13 +1100

    keep class names

--- webpack.config.js
+++ webpack.config.js
@@ -74,6 +74,10 @@ module.exports = {
     },
     optimization: {
         minimize: true,
-        minimizer: [new TerserPlugin()],
+        minimizer: [new TerserPlugin({
+            terserOptions: {
+                keep_fnames: true
+            }
+        })],
     }
 };
