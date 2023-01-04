676ab2e8e152ff34f94fe054ce9b79f54a2b5749
snowwhitedev <snowwhite91924@gmail.com>
2023-01-05 00:18:06 +0700

    feat: add TextEncoder global

--- src/wallet/WalletConnectProvider.ts
+++ src/wallet/WalletConnectProvider.ts
@@ -1,3 +1,7 @@
+import { TextEncoder, TextDecoder } from 'util';
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+
 import WalletConnectProvider from "@walletconnect/web3-provider";
 import UniversalProvider from "@walletconnect/universal-provider";
 
