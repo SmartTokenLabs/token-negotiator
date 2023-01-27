2adbd6f0bbd92283b8c0ffbd139d25f374d5cfd2
Michael Wallace <micwallace@gmx.com>
2023-01-27 14:19:05 +1100

    fix: remove redundant textEncoder
    - TextEncoder not required when explicitly importing umd module.
    - Add 'any' network parameter to ethers WC v2 instance.
    - Add Goerli & mumbai chain RPCs.

--- src/wallet/WalletConnectV2Provider.ts
+++ src/wallet/WalletConnectV2Provider.ts
@@ -1,15 +1,11 @@
-import { TextDecoder, TextEncoder } from 'text-encoding'
-global.TextEncoder = TextEncoder
-global.TextDecoder = TextDecoder
-
 import UniversalProvider from '@walletconnect/universal-provider/dist/index.umd'
 
 export const CUSTOM_RPCS_FOR_WC_V2 = {
 	1: 'https://ethereum.publicnode.com', // mainnet
-	// 5: 'https://eth-goerli.g.alchemy.com/v2/yVhq9zPJorAWsw-F87fEabSUl7cCU6z4', // Goerli
+	5: 'https://eth-goerli.g.alchemy.com/v2/yVhq9zPJorAWsw-F87fEabSUl7cCU6z4', // Goerli
 	// 11155111: 'https://sepolia.infura.io/v3/9f79b2f9274344af90b8d4e244b580ef', // Sepolia
 	137: 'https://polygon-rpc.com/', // Polygon
-	// 80001: 'https://polygon-mumbai.g.alchemy.com/v2/rVI6pOV4irVsrw20cJxc1fxK_1cSeiY0', // mumbai
+	80001: 'https://polygon-mumbai.g.alchemy.com/v2/rVI6pOV4irVsrw20cJxc1fxK_1cSeiY0', // mumbai
 	56: 'https://bsc-dataseed.binance.org/', // BSC,
 	// 97: 'https://data-seed-prebsc-1-s1.binance.org:8545', // BSC testnet
 	43114: 'https://api.avax.network/ext/bc/C/rpc', // Avalanche
