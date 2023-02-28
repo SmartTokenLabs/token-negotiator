a247cb525950e75c223510ddd7b405bc7fa10fed
Nick Taras <nicktaras@Nicks-MacBook-Pro-3.local>
2023-02-28 12:07:31 +1100

    added unit test scenario for tokens without the uid key

--- src/outlet/__tests__/outlet.spec.ts
+++ src/outlet/__tests__/outlet.spec.ts
@@ -43,4 +43,40 @@ describe('Outlet spec', () => {
 			'[{"ticketIdNumber":1},{"ticketIdNumber":2},{"ticketIdNumber":3},{"ticketIdNumber":5},{"ticketIdNumber":6}]',
 		)
 	})
+
+	test('ensure token duplicates are not present when key is missing from object', () => {
+		const outlet = new Outlet({
+			collectionID: 'devcon',
+			onChain: false,
+			title: 'Devcon',
+			image: 'https://raw.githubusercontent.com/TokenScript/token-negotiator/main/mock-images/devcon.svg',
+			tokenOrigin: 'http://localhost:3002/',
+			attestationOrigin: 'https://test.attestation.id/',
+			base64senderPublicKeys: {
+				'6': 'MIIBMzCB7AYHKoZIzj0CATCB4AIBATAsBgcqhkjOPQEBAiEA/////////////////////////////////////v///C8wRAQgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHBEEEeb5mfvncu6xVoGKVzocLBwKb/NstzijZWfKBWxb4F5hIOtp3JqPEZV2k+/wOEQio/Re0SKaFVBmcR9CP+xDUuAIhAP////////////////////66rtzmr0igO7/SXozQNkFBAgEBA0IABGMxHraqggr2keTXszIcchTjYjH5WXpDaBOYgXva82mKcGnKgGRORXSmcjWN2suUCMkLQj3UNlZCFWF10wIrrlw=',
+			},
+			base64attestorPubKey:
+				'MIIBMzCB7AYHKoZIzj0CATCB4AIBATAsBgcqhkjOPQEBAiEA/////////////////////////////////////v///C8wRAQgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHBEEEeb5mfvncu6xVoGKVzocLBwKb/NstzijZWfKBWxb4F5hIOtp3JqPEZV2k+/wOEQio/Re0SKaFVBmcR9CP+xDUuAIhAP////////////////////66rtzmr0igO7/SXozQNkFBAgEBA0IABL+y43T1OJFScEep69/yTqpqnV/jzONz9Sp4TEHyAJ7IPN9+GHweCX1hT4OFxt152sBN3jJc1s0Ymzd8pNGZNoQ=',
+			ticketIssuesUrlWebsitePrivateKey:
+				'MIICSwIBADCB7AYHKoZIzj0CATCB4AIBATAsBgcqhkjOPQEBAiEA/////////////////////////////////////v///C8wRAQgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHBEEEeb5mfvncu6xVoGKVzocLBwKb/NstzijZWfKBWxb4F5hIOtp3JqPEZV2k+/wOEQio/Re0SKaFVBmcR9CP+xDUuAIhAP////////////////////66rtzmr0igO7/SXozQNkFBAgEBBIIBVTCCAVECAQEEIM/T+SzcXcdtcNIqo6ck0nJTYzKL5ywYBFNSpI7R8AuBoIHjMIHgAgEBMCwGByqGSM49AQECIQD////////////////////////////////////+///8LzBEBCAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAQgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAcEQQR5vmZ++dy7rFWgYpXOhwsHApv82y3OKNlZ8oFbFvgXmEg62ncmo8RlXaT7/A4RCKj9F7RIpoVUGZxH0I/7ENS4AiEA/////////////////////rqu3OavSKA7v9JejNA2QUECAQGhRANCAARjMR62qoIK9pHk17MyHHIU42Ix+Vl6Q2gTmIF72vNpinBpyoBkTkV0pnI1jdrLlAjJC0I91DZWQhVhddMCK65c',
+		})
+
+		const issuerTokens = [
+			{ classId: 1 },
+			{ ticketIdNumber: 2 },
+			{ ticketIdNumber: 1 },
+			{ ticketIdNumber: 3 },
+			{ ticketIdNumber: 3 },
+			{ ticketIdNumber: 5 },
+			{ ticketIdNumber: 5 },
+			{ ticketIdNumber: 6 },
+			{ ticketIdNumber: 6 },
+		]
+
+		const noDuplicateTokens = outlet.removeDuplicatesByKey(issuerTokens, 'ticketIdNumber')
+
+		expect(JSON.stringify(noDuplicateTokens)).toBe(
+			'[{"classId":1},{"ticketIdNumber":2},{"ticketIdNumber":1},{"ticketIdNumber":3},{"ticketIdNumber":5},{"ticketIdNumber":6}]',
+		)
+	})
 })
