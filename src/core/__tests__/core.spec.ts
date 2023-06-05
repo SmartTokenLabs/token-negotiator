e6ef5e5a561717f637467dfaa6777aa70fd660f8
Nick Taras <nicktaras@Nicks-MacBook-Pro-3.local>
2023-06-05 14:27:56 +1000

    removal of "tn-ns" unit test this logic has been deprecated in the latest changes

--- src/core/__tests__/core.spec.ts
+++ src/core/__tests__/core.spec.ts
@@ -25,20 +25,4 @@ describe('Redirect flow', () => {
 		let url = new URL(window.location.href)
 		expect(new URLSearchParams(url.hash.substring(1)).get('tn-action')).toBe('111')
 	})
-
-	test('tokenNegotiatorClient add "tn-ns" param', async () => {
-		const messaging = new Messaging()
-		let request = {
-			origin: 'https://origin.url',
-			action: 'testaction',
-			data: { token: '222' },
-		}
-		let url = messaging.constructUrl('someID', request)
-
-		let url2 = new URL(url)
-		let urlParams = new URLSearchParams(url2.hash.substring(1))
-
-		expect(urlParams.get('tn-ns')).toEqual('1')
-		// expect(url2).toEqual('1')
-	})
 })
