e18c5f46dca34b435eaeaf03d1d30f4591700c11
Nick Taras <nicktaras@nicks-mbp-3.home>
2023-07-14 12:55:29 +0100

    fix for whitelist selection

--- src/outlet/whitelist.ts
+++ src/outlet/whitelist.ts
@@ -150,28 +150,44 @@ export class Whitelist {
 
 			document.body.insertAdjacentHTML('beforeend', content)
 
+			// document.getElementById('tn-access-accept').addEventListener('click', () => {
+			// 	const checks = document.getElementsByClassName('tn-permission-cb') as HTMLCollectionOf<HTMLInputElement>
+
+			// 	for (const check of checks) {
+			// 		const issuer = check.getAttribute('data-issuer')
+
+			// 		if (!this.storedWhitelist[origin]?.issuers) this.storedWhitelist[origin] = { issuers: [] }
+
+			// 		if (check.checked) {
+			// 			if (this.storedWhitelist[origin].issuers.indexOf(issuer) > -1) continue
+
+			// 			this.storedWhitelist[origin].issuers.push(issuer)
+			// 		} else {
+			// 			const index = this.storedWhitelist[origin].issuers.indexOf(issuer)
+
+			// 			if (!index) continue
+
+			// 			this.storedWhitelist[origin].issuers.splice(index, 1)
+			// 		}
+			// 	}
+
+			// 	this.saveWhitelist()
+
+			// 	resolve()
+			// })
+
 			document.getElementById('tn-access-accept').addEventListener('click', () => {
 				const checks = document.getElementsByClassName('tn-permission-cb') as HTMLCollectionOf<HTMLInputElement>
 
+				let updatedOriginIssuersList = []
+
 				for (const check of checks) {
 					const issuer = check.getAttribute('data-issuer')
-
 					if (!this.storedWhitelist[origin]?.issuers) this.storedWhitelist[origin] = { issuers: [] }
-
-					if (check.checked) {
-						if (this.storedWhitelist[origin].issuers.indexOf(issuer) > -1) continue
-
-						this.storedWhitelist[origin].issuers.push(issuer)
-					} else {
-						const index = this.storedWhitelist[origin].issuers.indexOf(issuer)
-
-						if (!index) continue
-
-						this.storedWhitelist[origin].issuers.splice(index, 1)
-					}
+					if (check.checked) updatedOriginIssuersList.push(issuer)
 				}
 
-				this.saveWhitelist()
+				this.storedWhitelist[origin].issuers = updatedOriginIssuersList
 
 				resolve()
 			})
