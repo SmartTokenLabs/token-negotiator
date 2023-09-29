7a5d629c10c6be3faaae17115be5dbbe61666d37
Nick Taras <nicktaras@Nicks-MacBook-Pro-3.local>
2023-09-29 12:35:26 +1000

    allow wallet to show when requested by config

--- src/client/index.ts
+++ src/client/index.ts
@@ -303,10 +303,11 @@ export class Client {
 				const blockChainUsed = issuerBlockChain === blockchain
 				const solanaEnabled = blockChainUsed && _blockchain === 'solana' // && typeof window.solana !== 'undefined'
 				const ultraEnabled = blockChainUsed && _blockchain === 'ultra' // && typeof window.ultra !== 'undefined'
+				const flowEnabled = blockChainUsed && _blockchain === 'flow'
 				const evmEnabled = blockChainUsed && _blockchain === 'evm' && !issuer.oAuth2options && !useOauth
 				const sociosEnabled = blockChainUsed && _blockchain === 'evm' && issuer.oAuth2options && useOauth
 				const fallBackToEVM = _blockchain === 'evm' && !issuerBlockChain && !useOauth
-				return solanaEnabled || ultraEnabled || evmEnabled || sociosEnabled || fallBackToEVM
+				return solanaEnabled || ultraEnabled || evmEnabled || sociosEnabled || flowEnabled || fallBackToEVM
 			}).length > 0
 		)
 	}
